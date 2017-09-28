67878623c8c3e54767f7141dfbc03fd81d27915c
Sven van Hal <sven@svenvanhal.nl>
2017-09-28 14:58:30 +0200

    Add board and committee archive pages.

---
+++ parts/post-type/archive-board.php
@@ -0,0 +1,9 @@
+<div class="row small-up-2 medium-up-3 large-up-4">
+    <?php
+    while (have_posts()) {
+        the_post(); ?>
+        <div class="column column-block">
+            <?php get_template_part('parts/post-type/excerpt', 'board'); ?>
+        </div>
+    <?php } ?>
+</div>
