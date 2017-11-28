4ce1154a60c75afeccdb6ceca9dc7ed62929d2bb
Sven van Hal <sven@svenvanhal.nl>
2017-11-28 15:20:59 +0100

    Add previous / next links for boards. (#11)

--- parts/post-type/single-board.php
+++ parts/post-type/single-board.php
@@ -1,13 +1,22 @@
 <?php
 $meta = get_post_custom(get_the_ID());
 $board_year = ! isset($meta['_board_year'][0]) ? '' : $meta['_board_year'][0];
-$with_photo = has_post_thumbnail() ? 'has-photo' : false;
+
+$prev = get_adjacent_post(false, '', true);
+$next = get_adjacent_post(false, '', false);
 ?>
-<article class="row roster-page post-content <?=$with_photo ?? ''?>">
 
-    <?php if ($with_photo) { ?>
+<article class="roster-page post-content">
 
-        <div class="column column-block">
+    <header class="row collapse" data-equalizer>
+
+        <nav class="small-2 column adjacent-link" data-equalizer-watch>
+            <?php if (! empty($next)) { ?>
+                <a class="button tiny" href="<?=get_permalink($next)?>"><i class="fa ch-arrow-left"></i></a>
+            <?php } ?>
+        </nav>
+
+        <div class="small-8 column column-block text-center" data-equalizer-watch>
             <?php
             the_title('<h1>', '</h1>');
 
@@ -15,6 +24,16 @@ $with_photo = has_post_thumbnail() ? 'has-photo' : false;
                 <p class="byline"><?=esc_html($board_year);?></p>
             <?php } ?>
         </div>
+
+        <nav class="small-2 column adjacent-link text-right" data-equalizer-watch>
+            <?php if (! empty($prev)) { ?>
+                <a class="button tiny" href="<?=get_permalink($prev)?>"><i class="fa ch-arrow-right"></i></a>
+            <?php } ?>
+        </nav>
+
+    </header>
+
+    <div class="row">
 
         <div class="large-5 column">
             <a href="<?=esc_url(get_the_post_thumbnail_url(null, 'large'))?>">
@@ -28,15 +47,5 @@ $with_photo = has_post_thumbnail() ? 'has-photo' : false;
             <?php the_content(); ?>
         </div>
 
-    <?php } else { ?>
-
-        <div class="column">
-            <?php
-            the_title('<h1>', '</h1>');
-            the_content();
-            ?>
-        </div>
-
-    <?php } ?>
-
+    </div>
 </article>
