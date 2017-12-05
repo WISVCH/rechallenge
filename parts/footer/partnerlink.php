963cb2f926abda5337952f62bd51cbcecfc2cd43
Sven van Hal <sven@svenvanhal.nl>
2017-12-05 12:40:48 +0100

    Limit amount of partnerlinks to nearest multiple of 5.

---
+++ parts/footer/partnerlink.php
@@ -0,0 +1,36 @@
+<div class="column">
+    <?php
+    $meta = get_post_custom(get_the_ID());
+    $link_url = empty($meta["_company_website"][0]) ? false : $meta["_company_website"][0];
+    $link_type = $meta["_link_to"][0] ?? false;
+
+    // Switch link type to post when 1) no link type is set or 2) the URL is not set
+    if (! $link_type || ($link_type == 'url' && empty($link_url))) {
+        $link_type = "post";
+    }
+
+    switch ($link_type) {
+        default:
+        case "post":
+            $link_text = 'View profile <i class="fa ch-arrow-right"></i>';
+            $link_url = get_permalink();
+            break;
+        case "url":
+            $link_text = 'Visit website <i class="fa ch-arrow-right"></i>';
+            $link_url = get_post_meta(get_the_ID(), "_company_website", true);
+            break;
+    }
+
+    $thumb = get_the_post_thumbnail_url(null, 'medium');
+    $style = $thumb ? ' style="background-image:url('.esc_url($thumb).')"' : '';
+    $class = 'class="'.($thumb ? 'img hidetext' : 'img').'"';
+    ?>
+    <a href="<?=esc_url($link_url)?>" title="<?php the_title_attribute(); ?>" class="partner">
+        <span <?=$class.$style?>><?php the_title(); ?></span>
+        <?php
+        $excerpt = get_the_excerpt();
+        if (! empty($excerpt)) { ?>
+            <span class="partner-excerpt"><?=$excerpt?><br><span class="button alt"><?=$link_text?></span></span>
+        <?php } ?>
+    </a>
+</div>
--- parts/footer/partners.php
+++ parts/footer/partners.php
@@ -14,54 +14,13 @@ if ($partners->have_posts()) { ?>
         <h1 class="small">Our partners</h1>
         <div class="row partners small-up-2 medium-up-4 large-up-5">
             <?php
-            while ($partners->have_posts()) {
-                $partners->the_post(); ?>
+            // Round to the nearest multiple of 5 (minimum of 5)
+            $max_partners = max(5, round($partners->found_posts / 5) * 5);
 
-                <div class="column">
-                    <?php
-                    $meta = get_post_custom(get_the_ID());
-                    $link_url = empty($meta["_company_website"][0]) ? false : $meta["_company_website"][0];
-                    $link_type = $meta["_link_to"][0] ?? false;
-
-                    // Switch link type to post when 1) no link type is set or 2) the URL is not set
-                    if (! $link_type || ($link_type == 'url' && empty($link_url))) {
-                        $link_type = "post";
-                    }
-
-                    switch ($link_type) {
-                        default:
-                        case "post":
-                            $link_text = 'View profile <i class="fa ch-arrow-right"></i>';
-                            $link_url = get_permalink();
-                            break;
-                        case "url":
-                            $link_text = 'Visit website <i class="fa ch-arrow-right"></i>';
-                            $link_url = get_post_meta(get_the_ID(), "_company_website", true);
-                            break;
-                    }
-
-                    $thumb = get_the_post_thumbnail_url(null, 'medium');
-                    $style = $thumb ? ' style="background-image:url('.esc_url($thumb).')"' : '';
-                    $class = 'class="'.($thumb ? 'img hidetext' : 'img').'"';
-                    ?>
-                    <a href="<?=esc_url($link_url)?>" title="<?php the_title_attribute(); ?>" class="partner">
-                        <span <?=$class.$style?>>
-                            <?php the_title(); ?>
-                        </span>
-
-                        <?php
-                        $excerpt = get_the_excerpt();
-                        if (! empty($excerpt)) {
-                            ?>
-                            <span class="partner-excerpt">
-                                <?=$excerpt?><br>
-                                <span class="button alt"><?=$link_text?></span>
-                            </span>
-                        <?php } ?>
-                    </a>
-                </div>
-
-            <?php } ?>
+            while ($partners->have_posts() && $partners->current_post + 1 < $max_partners) {
+                $partners->the_post();
+                get_template_part('parts/footer/partnerlink');
+            } ?>
         </div>
     </div>
 
