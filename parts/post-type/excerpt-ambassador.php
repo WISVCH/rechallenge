aa71e163159b98143cbff75d4ac790f98d88c086
Sven van Hal <sven@svenvanhal.nl>
2018-03-07 13:18:15 +0100

    Improve ambassador styling (#26)
    * Add support for Ambassadors.
    
    * Improve ambassador styling.

--- parts/post-type/excerpt-ambassador.php
+++ parts/post-type/excerpt-ambassador.php
@@ -1,5 +1,5 @@
 <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), "medium"); ?>
-<article class="excerpt-blockgrid excerpt-honorary-member">
+<article class="excerpt-blockgrid excerpt-ambassador">
     <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
         <div class="thumbnail nopad prop">
             <?php include(locate_template('parts/misc/thumbnail-proportional.php')); ?>
