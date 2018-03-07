2ee7e76b50bc327c603bd10ba23669e69c934352
Sven van Hal <sven@svenvanhal.nl>
2018-03-07 11:15:49 +0100

    Add support for Ambassadors. (#25)

---
+++ parts/post-type/excerpt-ambassador.php
@@ -0,0 +1,9 @@
+<?php $thumb = get_the_post_thumbnail_url(get_the_ID(), "medium"); ?>
+<article class="excerpt-blockgrid excerpt-honorary-member">
+    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        <div class="thumbnail nopad prop">
+            <?php include(locate_template('parts/misc/thumbnail-proportional.php')); ?>
+        </div>
+        <?php the_title('<h2>', '</h2>'); ?>
+    </a>
+</article>
