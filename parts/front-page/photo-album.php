d4df2cf9bae299c3c7f8853afe594b290c5be5e8
Sven van Hal <S.R.P.vanHal@student.tudelft.nl>
2020-03-18 10:05:27 +0100

    Temporarily remove latest galleries on front page.

--- parts/front-page/photo-album.php
+++ parts/front-page/photo-album.php
@@ -5,30 +5,7 @@
             <h1 class="small gallery-title">Photo galleries</h1>
         </div>
         <div class="small-4 column text-right">
-            <a class="button alt" href="https://flitcie.ch.tudelft.nl/" target="new">More <i class="fa ch-arrow-right"></i></a>
+            <a class="button alt" href="<?=site_url('media/photos')?>">View all <i class="fa ch-arrow-right"></i></a>
         </div>
     </div>
-
-    <div class="row small-up-1 medium-up-2 large-up-3 loading" data-wisvch-gallery>
-
-        <div class="column loading">Loading...</div>
-
-    </div>
-
-    <script type="text/javascript">
-        jQuery(function ($) {
-            $.getJSON("<?php echo admin_url("admin-ajax.php"); ?>?action=photo_album").done(function (data) {
-                var el = $("[data-wisvch-gallery]");
-                el.removeClass("loading").find(".loading").remove();
-                if (data.success === false) {
-                    el.append('<div class="column">Could not load photo album.</div>');
-                    return;
-                }
-                $.each(data, function (k, v) {
-                    el.append('<article class="column"><a target="new" class="gallery-item-wrapper" href="' + v.url + '"><div class="image-wrapper" style="background-image:url(' + v.cover + ')"></div><div class="caption"><div class="align-middle"><h1 class="small">' + v.title + '</h1></div></div></a></article>');
-                });
-            });
-        });
-    </script>
-
 </section>
