aadb4f3310c33bb2a4be089b1b525a583ea32a82
Bram van Kooten <bram.vankooten@hotmail.com>
2021-04-07 16:31:57 +0200

    Added functionality for displaying Committees page content to the committees page

--- parts/post-type/archive-committee.php
+++ parts/post-type/archive-committee.php
@@ -1,5 +1,16 @@
 <div class="row small-up-2 medium-up-3 large-up-4">
     <?php
+    $page = get_posts([
+        'name'      => 'committees',
+        'post_type' => 'page'
+    ]);
+
+    if ( $page )
+    {
+        echo $page[0]->post_content;
+    }
+
+
     while (have_posts()) {
         the_post(); ?>
         <div class="column column-block">
