747f5c5709b3fcdafc81e1a024891ac27084dbbe
Joep de Jong <joep@joepdejong.com>
2023-11-07 21:46:17 +0100

    Cleanup wp default endpoints

--- lib/Cleanup.php
+++ lib/Cleanup.php
@@ -44,6 +44,9 @@ class Cleanup
             add_filter('script_loader_src', [__CLASS__, 'remove_version_query_arg_amp'], 15, 1);
             add_filter('style_loader_src', [__CLASS__, 'remove_version_query_arg_amp'], 15, 1);
         }
+        
+        // Disable some endpoints for unauthenticated users
+        add_filter('rest_endpoints', 'disable_default_endpoints');
     }
 
     /**
@@ -115,5 +118,46 @@ class Cleanup
         $rqs = explode('&ver', $src);
 
         return $rqs[0];
+    }
+
+    /**
+     * Disable some endpoints for unauthenticated users
+     *
+     * @param $endpoints
+     * @return mixed
+     */
+    function disable_default_endpoints( $endpoints ) {
+        $endpoints_to_remove = array(
+            '/oembed/1.0',
+            '/wp/v2',
+            '/wp/v2/media',
+            '/wp/v2/types',
+            '/wp/v2/statuses',
+            '/wp/v2/taxonomies',
+            '/wp/v2/tags',
+            '/wp/v2/users',
+            '/wp/v2/comments',
+            '/wp/v2/settings',
+            '/wp/v2/themes',
+            '/wp/v2/blocks',
+            '/wp/v2/oembed',
+            '/wp/v2/posts',
+            '/wp/v2/pages',
+            '/wp/v2/block-renderer',
+            '/wp/v2/search',
+            '/wp/v2/categories'
+        );
+
+        if (!is_user_logged_in() ) {
+            foreach ($endpoints_to_remove as $rem_endpoint) {
+                // $base_endpoint = "/wp/v2/{$rem_endpoint}";
+                foreach ($endpoints as $maybe_endpoint => $object) {
+                    if (stripos( $maybe_endpoint, $rem_endpoint ) !== false) {
+                        unset( $endpoints[ $maybe_endpoint ] );
+                    }
+                }
+            }
+        }
+        return $endpoints;
     }
 }
